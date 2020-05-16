18f823f7f167ca93be04ecff1d91fa45b24a17fd
Jacqueline Wegert <weja1024@hs-karlsruhe.de>
2020-05-16 16:27:41 +0200

    Clean up.

--- src/JetsonNanoStatistics/jetsonNanoStatistics.py
+++ src/JetsonNanoStatistics/jetsonNanoStatistics.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# This file is part of the jetson_stats package (https://github.com/rbonghi/jetson_stats or http://rnext.it).
-# Copyright (c) 2019 Raffaello Bonghi.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 from jtop import jtop
 import time
 from enumComponent import Component
@@ -26,11 +8,10 @@ class JetsonNanoStatistics:
         with jtop() as jetson:
             while True:
                 self.__powerConsumptionInMilliwatts = jetson.stats["WATT"]
-                """
+                
                 print(self.getPowerConsumptionInMilliwattsFor(Component.CPU.value, ValueTypes.AVERAGE.value))
                 print(self.getPowerConsumptionInMilliwattsFor(Component.MAINBOARD.value, ValueTypes.AVERAGE.value))
                 print(self.getPowerConsumptionInMilliwattsFor(Component.GPU.value, ValueTypes.AVERAGE.value))
-                """
 
     """
     component - every possible component listed in enumComponent.py, e.g. Component.CPU.value to get the value of CPU
@@ -40,4 +21,3 @@ class JetsonNanoStatistics:
         return self.__powerConsumptionInMilliwatts[component][type]
 		
 statistics = JetsonNanoStatistics()
-# EOF
