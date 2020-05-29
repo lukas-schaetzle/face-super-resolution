050c6c05c829562b236bf884279ec81cdb57fdee
lschatzle <lukas.schatzle@logmein.com>
2020-05-29 13:37:56 +0200

    .

--- src/modules/face_detection/main.py
+++ src/modules/face_detection/main.py
@@ -1,19 +1,22 @@
-import torch, face_recognition
+import jetson.inference, jetson.utils
 from ..helper import FaceArea, upscaleTuple, resizeImage
 
 class FaceDetectionNet():
   def __init__(self):
-    self.model = 'cnn' if torch.cuda.is_available() else 'hog'
+    self.net = jetson.inference.detectNet("facenet", threshold=self._confidence)
 
   def infer(self, input_img):
     max_height, max_width = input_img.shape[:2]
 
-    small_frame, scale_factor = resizeImage(input_img, 640, 360)
+    input_img_rgba = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+    small_frame, scale_factor = resizeImage(input_img_rgba, 640, 360)
     reverse_scale_factor = 1 / scale_factor
-    face_locations = face_recognition.face_locations(small_frame, model=self.model)
+
+    cuda_img = jetson.utils.cudaFromNumpy(small_frame)
+    face_locations = self.net.Detect(small_frame, 640, 360)
 
     faces = []
-    for (top, right, bottom, left) in face_locations:
-      upscaled_coords = upscaleTuple(reverse_scale_factor, (left, top, right, bottom))
+    for face in face_locations:
+      upscaled_coords = upscaleTuple(reverse_scale_factor, (face.Left, face.Top, face.Right, face.Bottom))
       faces.append(FaceArea(upscaled_coords, max_dim=(max_width, max_height)))
     return faces
